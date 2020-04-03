40d8bded47961b1435e21ce96d89b8c556bf4a09
Roger <prrcanchanyaeur@globokas.com>
2020-04-03 16:30:50 -0500

    Primer commit -

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyAppView(),
+    );
+  }
+}
+
+class MyAppView extends StatefulWidget {
+  @override
+  _MyAppViewState createState() => _MyAppViewState();
+}
+
+class _MyAppViewState extends State<MyAppView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
