fc20d375a6088ef0a5f836b3dc68cab364664d6a
Roger <prrcanchanyaeur@globokas.com>
2020-04-03 21:14:01 -0500

    Ejemplo base completo

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:app_instagram/post.dart';
+import 'package:app_instagram/story.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+
+import 'feed.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MyAppView(),
     );
   }
@@ -17,10 +23,91 @@ class MyAppView extends StatefulWidget {
 }
 
 class _MyAppViewState extends State<MyAppView> {
+  var _pages = [
+    Feed(),
+    Feed(),
+    Feed(),
+    Feed(),
+    Feed(),
+  ];
+
+  int currentPage = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Color(0xFFEEEEEE),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Color(0xFFEEEEEE),
+        brightness: Brightness.light,
+        centerTitle: true,
+        title: Text(
+          "Instagram",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Feather.camera,
+            color: Colors.black,
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Feather.tv,
+              color: Colors.black,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              FontAwesome.send_o,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+
+      body: _pages[currentPage],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentPage,
+        onTap: (i){
+          setState(() {
+            currentPage = i;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.black,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Foundation.home),
+            title: Text("Feed")
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Feather.search),
+            title: Text("Search")
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Feather.plus_square),
+            title: Text("Upload")
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Feather.heart),
+            title: Text("Likes")
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Feather.user),
+            title: Text("Account")
+          ),
+        ],
+      ),
     );
   }
 }
